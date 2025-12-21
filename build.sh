017bb0ad0105ab7cbc044b7369db9934050ab1d9
Jake Taylor <jake.e.taylor1@gmail.com>
2025-12-21 01:45:57 -0800

    fix build script paths for web deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 flutter build web --base-href "/apps/rent_vs_buy/"
 echo "Removing current web build"
-rm -rf ~/Documents/jakee417.github.io/apps/rent_vs_buy/
+rm -rf ~/jakee417.github.io/apps/rent_vs_buy/
 echo "Copying new web build"
-cp -r build/web/ ~/Documents/jakee417.github.io/apps/rent_vs_buy/
+cp -r build/web/ ~/jakee417.github.io/apps/rent_vs_buy/

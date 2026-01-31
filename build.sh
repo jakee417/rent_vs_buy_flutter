2f6fb89171a7258d3a13bbff95c875b6837bfcd1
Jake Taylor <43712099+jakee417@users.noreply.github.com>
2026-01-31 09:34:56 -0800

    fix: Update paths in build script for web deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 flutter build web --base-href "/apps/finance_calculator/"
 echo "Removing current web build"
-rm -rf ~/jakee417.github.io/apps/finance_calculator/
+rm -rf ~/Documents/jakee417.github.io/apps/finance_calculator/
 echo "Copying new web build"
-cp -r build/web/ ~/jakee417.github.io/apps/finance_calculator/
+cp -r build/web/ ~/Documents/jakee417.github.io/apps/finance_calculator/
